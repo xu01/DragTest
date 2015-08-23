f043ad8b6dd9870cfda1647ea189d3e5a53b0416
xu01 <lingyi_xu@hotmail.com>
2015-08-23 10:58:57 +0800

    init develop

---
+++ DragTest/Classes/Controllers/DTMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  DTMainViewController.h
+//  DragTest
+//
+//  Created by xu lingyi on 8/23/15.
+//  Copyright (c) 2015 xu01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTMainViewController : UIViewController
+
+@end
---
+++ DragTest/Classes/Controllers/DTMainViewController.m
@@ -0,0 +1,39 @@
+//
+//  DTMainViewController.m
+//  DragTest
+//
+//  Created by xu lingyi on 8/23/15.
+//  Copyright (c) 2015 xu01. All rights reserved.
+//
+
+#import "DTMainViewController.h"
+
+@interface DTMainViewController ()
+
+@end
+
+@implementation DTMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
